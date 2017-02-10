b88301433f883224348f5c87abf7f361c561735c
Ubuntu <ubuntu@ip-172-31-9-91.us-west-1.compute.internal>
2017-02-10 00:31:38 +0000

    adding styling

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '40h^1x#ofr6^3_s@l55y-e$jtpzxnk_0(5f=y+60@%^$)&6$k@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.193.43.37']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
